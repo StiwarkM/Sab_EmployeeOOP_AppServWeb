da4c1ce1530370570582bbc00ad99deae15e31fb
St1w4rk <stiwarkmedina00@gmail.com>
2023-09-16 16:53:33 -0500

    New abstract class Employee created

--- EmployeeOOP/Program.cs
+++ EmployeeOOP/Program.cs
@@ -1,10 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeOOP.Classes;
 
-Console.WriteLine("OOP APLICATION");
-Console.WriteLine("--------------");
+try
+{
+    // Declaracion de variables
+    int day, month, year;
 
-//Inicializacion de la clase Date
-Date dateObject = new Date(4,5,5655214);
-Console.WriteLine(dateObject.ToString());
+	Console.WriteLine("OOP APLICATION");
+	Console.WriteLine("--------------");
+
+    // Ingreso de datos
+	Console.Write("Ingresar el día: ");
+	day = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el mes: ");
+    month = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Ingresar el año: ");
+    year = Convert.ToInt32(Console.ReadLine());
+
+    //Inicializacion de la clase Date
+    Date dateObject = new Date(day,month,year);
+    Console.WriteLine("\n");//Salto de linea
+	Console.WriteLine(dateObject.ToString());
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
 
